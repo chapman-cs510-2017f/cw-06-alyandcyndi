f89b8fefb45bd3ab17af79591530f755fe68f1c3
Cynthia Parks <cparks@chapman.edu>
2017-10-04 02:22:46 +0000

    started code

---
+++ cplane-np.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+###
+# Name: Aly Baughman, Cynthia Parks
+# Student ID: 1923165, 2303535
+# Email: baugh107@mail.chapman.edu, cparks@chapman.edu
+# Course: CS510 Fall 2017
+# Assignment: Classwork 6
+###
+
+from abscplane import AbsComplexPlane
+
+class ArrayComplexPlane(AbsComplexPlane):
+    
+    
